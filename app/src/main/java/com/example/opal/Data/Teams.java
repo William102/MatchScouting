fb03f8536390c3742a575d1d7f7cc6f8c415b99f
WIlliam Iheanacho <williami@vandenrobotics.com>
2021-12-17 17:00:08 -0800

    Working on database

---
+++ app/src/main/java/com/example/opal/Data/Teams.java
@@ -0,0 +1,12 @@
+package com.example.opal.Data;
+
+public class Teams {
+
+    public String author;
+    public String title;
+
+    public Teams(String author, String title) {
+        // ...
+    }
+
+}
--- app/src/main/java/com/example/opal/MainActivity.java
+++ app/src/main/java/com/example/opal/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.opal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button matchFragment = findViewById(R.id.match_fragment);
-        matchFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
     }
 }
